cd3ea6a6f3e2b3a0175086b1496addbcd27000b0
Benno Lossin <lossin@kernel.org>
2025-05-24 16:39:01 +0200

    add cpp_ref example

---
+++ examples/cpp_ref/cpp_ref.rs
@@ -0,0 +1,50 @@
+use std::ops::Receiver;
+
+use field_projection::{
+    compat::{ProjectableExt, Safe},
+    marker::Field,
+    ops::{Project, ProjectMut, Projectable, SafeProject},
+};
+
+pub struct CppMutRef<T: ?Sized>(*mut T);
+
+impl<T: ?Sized> CppMutRef<T> {
+    pub fn write(&mut self, value: T)
+    where
+        T: Sized + Copy,
+    {
+        unsafe { self.0.write(value) }
+    }
+}
+
+impl<T: ?Sized> Receiver for CppMutRef<T> {
+    type Target = T;
+}
+
+impl<T: ?Sized> Projectable for CppMutRef<T> {
+    type Inner = T;
+}
+
+unsafe impl<T: ?Sized> SafeProject for CppMutRef<T> {}
+
+unsafe impl<T: ?Sized> ProjectableExt for CppMutRef<T> {
+    type Safety = Safe;
+}
+
+unsafe impl<T, F> ProjectMut<F> for CppMutRef<T>
+where
+    F: Field<Base = T>,
+    F::Type: Sized,
+{
+    type OutputMut<'a>
+        = CppMutRef<F::Type>
+    where
+        Self: 'a;
+
+    unsafe fn project_mut<'a>(this: *mut Self) -> Self::OutputMut<'a>
+    where
+        Self: 'a,
+    {
+        CppMutRef(unsafe { <*mut T as Project<F>>::project(&raw const (*this).0) })
+    }
+}
---
+++ examples/cpp_ref/main.rs
@@ -0,0 +1,27 @@
+#![feature(arbitrary_self_types)]
+
+use field_projection::compat::{HasFields, p, start_proj};
+
+mod cpp_ref;
+use cpp_ref::CppMutRef;
+
+// Shared between C++ and Rust.
+#[derive(HasFields)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Point {
+    pub fn set_x(mut self: CppMutRef<Self>, value: i32) {
+        start_proj!(mut self);
+        p!(@mut self->x).write(value);
+    }
+
+    pub fn set_y(mut self: CppMutRef<Self>, value: i32) {
+        start_proj!(mut self);
+        p!(@mut self->y).write(value);
+    }
+}
+
+fn main() {}
