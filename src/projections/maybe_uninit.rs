40a4ed54c036ce42232635dd988114d86a740e92
Benno Lossin <lossin@kernel.org>
2025-05-29 18:35:31 +0200

    add read-only projections to `MaybeUninit`

--- src/projections/maybe_uninit.rs
+++ src/projections/maybe_uninit.rs
@@ -1,11 +1,58 @@
 use core::mem::MaybeUninit;
 
+impl<T> Projectable for &MaybeUninit<T> {
+    type Inner = T;
+}
+
 impl<T> Projectable for &mut MaybeUninit<T> {
     type Inner = T;
 }
 
 // SAFETY: no additional safety requirements on `Project[Mut]::project[_mut]`.
+unsafe impl<T> SafeProject for &MaybeUninit<T> {}
+
+// SAFETY: no additional safety requirements on `Project[Mut]::project[_mut]`.
 unsafe impl<T> SafeProject for &mut MaybeUninit<T> {}
+
+// No additional safety requirements for `project`.
+impl<'a, T, F> Project<F> for &'a MaybeUninit<T>
+where
+    F: Field<Base = T>,
+    F::Type: Sized + 'a,
+{
+    type Output<'b>
+        = &'b MaybeUninit<F::Type>
+    where
+        Self: 'b;
+
+    unsafe fn project<'b>(this: *const Self) -> Self::Output<'a>
+    where
+        Self: 'b,
+    {
+        let ptr: *const MaybeUninit<T> = unsafe { this.read() };
+        unsafe { &*ptr.byte_add(F::OFFSET).cast() }
+    }
+}
+
+// No additional safety requirements for `project`.
+impl<'a, T, F> Project<F> for &'a mut MaybeUninit<T>
+where
+    F: Field<Base = T>,
+    F::Type: Sized + 'a,
+{
+    type Output<'b>
+        = &'b MaybeUninit<F::Type>
+    where
+        Self: 'b;
+
+    unsafe fn project<'b>(this: *const Self) -> Self::Output<'a>
+    where
+        Self: 'b,
+    {
+        let ptr: *const MaybeUninit<T> = unsafe { this.read() };
+        unsafe { &*ptr.byte_add(F::OFFSET).cast() }
+    }
+}
 
 // No additional safety requirements for `project_mut`.
 impl<'a, T, F> ProjectMut<F> for &'a mut MaybeUninit<T>
@@ -28,6 +75,10 @@ where
 }
 
 // Compat
+
+unsafe impl<T> compat::ProjectableExt for &MaybeUninit<T> {
+    type Safety = compat::Safe;
+}
 
 unsafe impl<T> compat::ProjectableExt for &mut MaybeUninit<T> {
     type Safety = compat::Safe;
