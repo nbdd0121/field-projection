6b6e41d8f09398d7b61c1c5bca05b3fa3a04a39c
Benno Lossin <lossin@kernel.org>
2025-08-11 14:29:13 +0200

    allow nonnull projections when `T: ?Sized`

--- src/projections/non_null.rs
+++ src/projections/non_null.rs
@@ -7,7 +7,7 @@ impl<T: ?Sized> Projectable for NonNull<T> {
 // Additional safety requirements for `project`:
 // * The pointer pointed at by `this` must point to an allocated object at least as large as
 //   `size_of::<T>()`.
-impl<T, F> Project<F> for NonNull<T>
+impl<T: ?Sized, F> Project<F> for NonNull<T>
 where
     F: UnalignedField<Base = T>,
     F::Type: Sized,
@@ -27,6 +27,6 @@ where
 
 // Compat
 
-unsafe impl<T> compat::ProjectableExt for NonNull<T> {
+unsafe impl<T: ?Sized> compat::ProjectableExt for NonNull<T> {
     type Safety = compat::Unsafe;
 }
